7d2d934a2b17d1944698ad591d8a1e5717065bfb
Crazycaleb <52386586+Crazycaleb@users.noreply.github.com>
2024-10-13 23:38:16 -0400

    Model finished!

--- Assets/AnswerSmashScript.cs
+++ Assets/AnswerSmashScript.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 using System;
 using Newtonsoft.Json;
 using UnityEngine.UI;
+using Rnd = UnityEngine.Random;
+
 
 public class AnswerSmashScript : MonoBehaviour {
 
@@ -20,7 +22,10 @@ public class AnswerSmashScript : MonoBehaviour {
     public MeshRenderer[] selectionOutlines;
 
     public GameObject[] bulbs;
+    public GameObject[] allBulbs;
+    public GameObject[] largeDisplays;
     public Light[] bulbLights;
+    public Light flashbang;
 
     public Light spotlight;
 
@@ -44,9 +49,11 @@ public class AnswerSmashScript : MonoBehaviour {
     int moduleId;
     private bool moduleSolved;
 
-    void Awake()
+    void Start()
     {
         moduleId = moduleIdCounter++;
+        flashbang.enabled = false;
+        flashbang.intensity = 0;
         foreach (KMSelectable obj in buttons)
         {
             KMSelectable pressed = obj;
@@ -56,17 +63,14 @@ public class AnswerSmashScript : MonoBehaviour {
             focused = true;
         GetComponent<KMSelectable>().OnFocus += delegate () { focused = true; };
         GetComponent<KMSelectable>().OnDefocus += delegate () { focused = false; };
-    }
-
-    void Start()
-    {
         StartCoroutine(LoadContentAndGenerate());
         StartCoroutine(lightCycle());
+
     }
 
     void Update()
     {
-        if (moduleSolved != true && loading != true && focused != false)
+        if (moduleSolved != true && loading != true && focused != false && subButton != true)
         {
             if (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.RightShift))
                 shiftDown = true;
@@ -126,55 +130,153 @@ public class AnswerSmashScript : MonoBehaviour {
                     List<string> smashes2 = GetSmashedAnswers(displays[2].text, displays[1].text);
                     if (displays[1].text == "" || displays[2].text == "")
                     {
+                        selectionOutlines[0].enabled = false;
+                        selectionOutlines[1].enabled = false;
                         Debug.LogFormat("[Answer Smash #{0}] Your answers cannot be blank, strike!", moduleId);
-                        audio.PlaySoundAtTransform("SkipToDie", transform);
+                        audio.PlaySoundAtTransform("SkipToDie", transform); //Special little case I had in mind when making the model if one of the answers was blank (Crazycaleb).
                         yield return new WaitForSeconds(4.0f);
                         StartCoroutine(submissionAnim());
                         yield return new WaitForSeconds(6.5f);
+                        audio.PlaySoundAtTransform("CouldntResist", transform);
                         GetComponent<KMBombModule>().HandleStrike();
+                        buttons[0].gameObject.SetActive(true);
+                        buttons[1].gameObject.SetActive(true);
+                        largeDisplays[0].SetActive(true);
+                        displays[0].gameObject.SetActive(true);
+                        displays[1].transform.localPosition = new Vector3(-0.015f, -0.0033f, -0.015f);
+                        displays[2].transform.localPosition = new Vector3(-0.015f, -0.034f, -0.015f);
+                        largeDisplays[1].transform.localPosition = new Vector3(-0.015f, 0.012f, -0.0033f);
+                        largeDisplays[2].transform.localPosition = new Vector3(-0.015f, 0.012f, -0.034f);  
+                        selectionOutlines[0].enabled = true;
+                        for (int i = 0; i < allBulbs.Length; i++)
+                        {
+                            allBulbs[i].SetActive(true);
+                        }
+                        subButton = false;
+                        StartCoroutine(lightCycle());
                     }
                     else if (entries.All(x => x.Name != displays[1].text))
-                    {                        
+                    {
+                        selectionOutlines[0].enabled = false;
+                        selectionOutlines[1].enabled = false;
                         StartCoroutine(submissionAnim());
                         yield return new WaitForSeconds(6.5f);
                         Debug.LogFormat("[Answer Smash #{0}] {1} is not a valid repository entry, strike!", moduleId, displays[1].text);
                         audio.PlaySoundAtTransform("CouldntResist", transform);
                         GetComponent<KMBombModule>().HandleStrike();
+                        buttons[0].gameObject.SetActive(true);
+                        buttons[1].gameObject.SetActive(true);
+                        largeDisplays[0].SetActive(true);
+                        displays[0].gameObject.SetActive(true);
+                        displays[1].transform.localPosition = new Vector3(-0.015f, -0.0033f, -0.015f);
+                        displays[2].transform.localPosition = new Vector3(-0.015f, -0.034f, -0.015f);
+                        largeDisplays[1].transform.localPosition = new Vector3(-0.015f, 0.012f, -0.0033f);
+                        largeDisplays[2].transform.localPosition = new Vector3(-0.015f, 0.012f, -0.034f);
+                        selectionOutlines[0].enabled = true;
+                        for (int i = 0; i < allBulbs.Length; i++)
+                        {
+                            allBulbs[i].SetActive(true);
+                        }
+                        subButton = false;
+                        StartCoroutine(lightCycle());
                     }
                     else if (entries.All(x => x.Name != displays[2].text))
-                    {                        
+                    {
+                        selectionOutlines[0].enabled = false;
+                        selectionOutlines[1].enabled = false;
                         StartCoroutine(submissionAnim());
                         yield return new WaitForSeconds(6.5f);
                         Debug.LogFormat("[Answer Smash #{0}] {1} is not a valid repository entry, strike!", moduleId, displays[2].text);
                         audio.PlaySoundAtTransform("CouldntResist", transform);
                         GetComponent<KMBombModule>().HandleStrike();
+                        largeDisplays[0].SetActive(true);
+                        displays[0].gameObject.SetActive(true);
+                        buttons[0].gameObject.SetActive(true);
+                        buttons[1].gameObject.SetActive(true);
+                        displays[1].transform.localPosition = new Vector3(-0.015f, -0.0033f, -0.015f);
+                        displays[2].transform.localPosition = new Vector3(-0.015f, -0.034f, -0.015f);
+                        largeDisplays[1].transform.localPosition = new Vector3(-0.015f, 0.012f, -0.0033f);
+                        largeDisplays[2].transform.localPosition = new Vector3(-0.015f, 0.012f, -0.034f);
+                        selectionOutlines[0].enabled = true;
+                        for (int i = 0; i < allBulbs.Length; i++)
+                        {
+                            allBulbs[i].SetActive(true);
+                        }
+                        subButton = false;
+                        StartCoroutine(lightCycle());
                     }
                     else if (displays[1].text == displays[2].text)
-                    {                        
+                    {
+                        selectionOutlines[0].enabled = false;
+                        selectionOutlines[1].enabled = false;
                         StartCoroutine(submissionAnim());
                         yield return new WaitForSeconds(6.5f);
                         Debug.LogFormat("[Answer Smash #{0}] Your answers cannot be the same repository entry, strike!", moduleId);
                         audio.PlaySoundAtTransform("CouldntResist", transform);
                         GetComponent<KMBombModule>().HandleStrike();
+                        buttons[0].gameObject.SetActive(true);
+                        buttons[1].gameObject.SetActive(true);
+                        largeDisplays[0].SetActive(true);
+                        displays[0].gameObject.SetActive(true);
+                        displays[1].transform.localPosition = new Vector3(-0.015f, -0.0033f, -0.015f);
+                        displays[2].transform.localPosition = new Vector3(-0.015f, -0.034f, -0.015f);
+                        largeDisplays[1].transform.localPosition = new Vector3(-0.015f, 0.012f, -0.0033f);
+                        largeDisplays[2].transform.localPosition = new Vector3(-0.015f, 0.012f, -0.034f);
+                        selectionOutlines[0].enabled = true;
+                        for (int i = 0; i < allBulbs.Length; i++)
+                        {
+                            allBulbs[i].SetActive(true);
+                        }
+                        subButton = false;
+                        StartCoroutine(lightCycle());
                     }
                     else if (smashes1.Contains(generatedSmash) || smashes2.Contains(generatedSmash))
-                    {                        
+                    {
+                        selectionOutlines[0].enabled = false;
+                        selectionOutlines[1].enabled = false;
                         StartCoroutine(submissionAnim());
                         yield return new WaitForSeconds(6.5f);
                         moduleSolved = true;
-                        selectionOutlines[0].enabled = false;
-                        selectionOutlines[1].enabled = false;
                         Debug.LogFormat("[Answer Smash #{0}] Module solved!", moduleId);
                         audio.PlaySoundAtTransform("DieSolveSound", transform);
                         GetComponent<KMBombModule>().HandlePass();
+                        largeDisplays[1].gameObject.SetActive(false);
+                        largeDisplays[2].gameObject.SetActive(false);
+                        displays[1].gameObject.SetActive(false);
+                        displays[2].gameObject.SetActive(false);
+                        largeDisplays[0].SetActive(true);
+                        largeDisplays[0].transform.localPosition = new Vector3(0f, 0.012f, 0f);
+                        displays[0].gameObject.SetActive(true);
+                        displays[0].transform.localPosition = new Vector3(0f, 0f, -0.015f);
+                        for (int i = 0; i < allBulbs.Length; i++)
+                        {
+                            allBulbs[i].SetActive(true);
+                        }
                     }
                     else
                     {
+                        selectionOutlines[0].enabled = false;
+                        selectionOutlines[1].enabled = false;
                         StartCoroutine(submissionAnim());
                         yield return new WaitForSeconds(6.5f);
                         Debug.LogFormat("[Answer Smash #{0}] Your answers cannot make the displayed answer smash, strike!", moduleId);
                         audio.PlaySoundAtTransform("CouldntResist", transform);
                         GetComponent<KMBombModule>().HandleStrike();
+                        largeDisplays[0].SetActive(true);
+                        displays[0].gameObject.SetActive(true);
+                        buttons[0].gameObject.SetActive(true);
+                        buttons[1].gameObject.SetActive(true);
+                        displays[1].transform.localPosition = new Vector3(-0.015f, -0.0033f, -0.015f);
+                        displays[2].transform.localPosition = new Vector3(-0.015f, -0.034f, -0.015f);
+                        largeDisplays[1].transform.localPosition = new Vector3(-0.015f, 0.012f, -0.0033f);
+                        largeDisplays[2].transform.localPosition = new Vector3(-0.015f, 0.012f, -0.034f);
+                        selectionOutlines[0].enabled = true;
+                        for (int i = 0; i < allBulbs.Length; i++)
+                        {
+                            allBulbs[i].SetActive(true);
+                        }
+                        subButton = false;
+                        StartCoroutine(lightCycle());
                     }
                 }
             }
@@ -182,12 +284,14 @@ public class AnswerSmashScript : MonoBehaviour {
     }
 
     IEnumerator lightCycle(){
-        for (int i = 0; i < bulbLights.Length; i++){
+        flashbang.enabled = false;
+        flashbang.intensity = 0;
+        for (int i = 0; i < bulbLights.Length; i++){ //Start with everything off
             bulbLights[i].enabled = false;
             bulbs[i].GetComponent<MeshRenderer>().material = bulbStates[0];
         }
 
-        while (subButton == false){
+        while (subButton == false){ //If we haven't pressed submit button, do the light cycle
             for (int i = 0; i < bulbLights.Length; i++)
             {
                 if(cyclePos == 0){
@@ -232,11 +336,11 @@ public class AnswerSmashScript : MonoBehaviour {
                 }
             }
             yield return new WaitForSeconds(0.15f);
-            cyclePos += 2;
-            cyclePos = cyclePos % 3;
+            cyclePos += 2; //So it'll rotate clockwise, instead of counter.
+            cyclePos = cyclePos % 3; 
 
         }            
-        for (int i = 0; i < bulbLights.Length; i++)
+        for (int i = 0; i < bulbLights.Length; i++) //Once the submit button is pressed, turn the lights off.
         {
             bulbLights[i].enabled = false;
             bulbs[i].GetComponent<MeshRenderer>().material = bulbStates[0];
@@ -245,13 +349,80 @@ public class AnswerSmashScript : MonoBehaviour {
 
     IEnumerator submissionAnim(){
         audio.PlaySoundAtTransform("Lights_Camera_Action", transform);
-        yield return new WaitForSeconds(1.0f);
-        //SceneManager.Instance.GameplayState.Room.CeilingLight.TurnOff(false);
-        yield return new WaitForSeconds(2.2f);
-        spotlight.enabled = true;
+        yield return new WaitForSeconds(1.5f);
+        if (!Application.isEditor){ //If we aren't in Unity, turn lights off.
+            SceneManager.Instance.GameplayState.Room.CeilingLight.TurnOff(false); 
+        }
+        for (int i = 0; i < allBulbs.Length; i++){ //Remove Bulbs during the animation while it is dark in the room
+            allBulbs[i].SetActive(false);
+        }
+        largeDisplays[0].SetActive(false);
+        displays[0].gameObject.SetActive(false);
+        buttons[0].gameObject.SetActive(false);
+        buttons[1].gameObject.SetActive(false);
+        displays[1].transform.localPosition = new Vector3(0f, 0.03f, -0.015f);
+        displays[2].transform.localPosition = new Vector3(0f, -0.03f, -0.015f);
+        largeDisplays[1].transform.localPosition = new Vector3(0f, 0.012f, 0.03f);
+        largeDisplays[2].transform.localPosition = new Vector3(0f, 0.012f, -0.03f);
+        yield return new WaitForSeconds(1.7f);        
+        spotlight.enabled = true; //Turn spotlight on
+        StartCoroutine(shakeshakeshakeSenora());
         yield return new WaitForSeconds(3.3f);
-        //SceneManager.Instance.GameplayState.Room.CeilingLight.TurnOff(true);
-        spotlight.enabled = false;
+        if (!Application.isEditor)//After submission, turn lights back on. (Out of the eternal darkness)
+        {
+            SceneManager.Instance.GameplayState.Room.CeilingLight.TurnOn(true);
+        }
+        spotlight.enabled = false; //Spotlight off
+    }
+
+    IEnumerator shakeshakeshakeSenora(){
+        float elapsedTime = 0f;
+        bool flashed = false;
+        float duration = 3.2f;
+        while (elapsedTime < duration)
+        {
+            elapsedTime += Time.deltaTime;
+            displays[1].transform.localEulerAngles = new Vector3(0, Mathf.Sin(elapsedTime / (elapsedTime < 3 ? .1f : 3f) * Mathf.PI * 2) * 1f, 0);
+            displays[2].transform.localEulerAngles = new Vector3(0, Mathf.Sin(elapsedTime / (elapsedTime < 3 ? .1f : 3f) * Mathf.PI * 2) * 1f, 0);
+            largeDisplays[1].transform.localEulerAngles = new Vector3(0, Mathf.Sin(elapsedTime / (elapsedTime < 3 ? .1f : 3f) * Mathf.PI * 2) * 1f, 0);
+            largeDisplays[2].transform.localEulerAngles = new Vector3(0, Mathf.Sin(elapsedTime / (elapsedTime < 3 ? .1f : 3f) * Mathf.PI * 2) * 1f, 0);
+            displays[1].transform.localPosition = new Vector3(0f, Mathf.Lerp(0.03f, 0f, elapsedTime / duration), -0.015f);
+            displays[2].transform.localPosition = new Vector3(0f, Mathf.Lerp(-0.03f, 0f, elapsedTime / duration), -0.015f);
+            largeDisplays[1].transform.localPosition = new Vector3(0f, 0.012f, Mathf.Lerp(0.03f, 0f, elapsedTime / duration));
+            largeDisplays[2].transform.localPosition = new Vector3(0f, 0.012f, Mathf.Lerp(-0.03f, 0f, elapsedTime / duration));
+            if (elapsedTime >= 2.2f && !flashed){
+                StartCoroutine(FLASHAHHAHHSAVIOROFTHEUNIVERSE());
+                flashed = true;
+            }
+            yield return null;
+        }
+        displays[1].transform.localEulerAngles = new Vector3(0, 0, 0);
+        displays[2].transform.localEulerAngles = new Vector3(0, 0, 0);
+        largeDisplays[1].transform.localEulerAngles = new Vector3(0, 0, 0);
+        largeDisplays[2].transform.localEulerAngles = new Vector3(0, 0, 0);
+    }
+
+    IEnumerator FLASHAHHAHHSAVIOROFTHEUNIVERSE()
+    {
+        flashbang.enabled = true;
+        var duration = 1f;
+        var elapsed = 0f;
+        var flashSize = 20f;
+        while (elapsed < duration)
+        {
+            flashbang.intensity = Easing.OutQuad(elapsed, 0f, flashSize, duration);
+            yield return null;
+            elapsed += Time.deltaTime;
+        }
+        // Do your code switch thing here
+        elapsed = 0f;
+        while (elapsed < duration)
+        {
+            flashbang.intensity = Easing.OutQuad(elapsed, flashSize, 0f, duration);
+            yield return null;
+            elapsed += Time.deltaTime;
+        }
+        flashbang.enabled = false;
     }
 
     List<string> GetSmashedAnswers(string ans1, string ans2)
